79883043c5b65a8f5742f8365030d55f0afd2a67
Saras Paudel <saraspaudel@utexas.edu>
2019-02-17 08:30:55 -0600

    Generated build production file from Angular CLI specifically for Github Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCrUBL-9mBCGaP4YvfREVGS3apHLYLT5xA",
+    authDomain: "sayyestodallas-hackdfw2019.firebaseapp.com",
+    databaseURL: "https://sayyestodallas-hackdfw2019.firebaseio.com",
+    projectId: "sayyestodallas-hackdfw2019",
+    storageBucket: "sayyestodallas-hackdfw2019.appspot.com",
+    messagingSenderId: "1019587010123"
+  }
 };
